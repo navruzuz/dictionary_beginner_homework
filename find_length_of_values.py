b6720ef359fc122140df5e90a65a1b35579778aa
navruzuz <hamrayevnavruzbek9906@gmail.com>
2023-08-14 06:12:41 +0500

    bajarildi

--- find_length_of_values.py
+++ find_length_of_values.py
@@ -6,4 +6,13 @@ def find_length_of_values(data: dict) -> int:
     Returns:
         int: The sum of the length of all values in the dictionary.
     """
-    return 
+    # for i,item in enumerate (data):
+    #     print(i)
+    return data.popitem()
+data = {
+    "x": "23",
+    "3": "y", 
+    "z": "5", 
+    7:'a'
+  }
+print(find_length_of_values(data))
